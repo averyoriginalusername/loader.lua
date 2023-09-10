cb6ee250ec1ec0797c46431df0bd0862accb807c
averyoriginalusername <144381271+averyoriginalusername@users.noreply.github.com>
2023-09-10 16:50:41 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,46 +1,20 @@
-local http_request = game:HttpGet('https://pastebin.com/raw/ymTEAzmy1')
-local scriptKeys = game:GetService('HttpService'):JSONDecode(http_request)
-local ClientHWID = nil
-local HasKey = nil
-
-local CheckExecutor = function(Table)
-	for name,_ in pairs(Table) do
-		if identifyexecutor():match(tostring(name)) then
-			return true
-		else
+do	
+	local GameList = {
+		[13747403394] = loadstring(game:HttpGet("https://raw.githubusercontent.com/averyoriginalusername/main/main/Rogue-Lineage-Richest-Minion.lua"));
+	}
+	local ClientHWID = nil
+	local CheckExecutor = function(Table)
+		for name,_ in pairs(Table) do
+			if identifyexecutor():match(tostring(name)) then
+				return true
+			end
 			return false
 		end
 	end
-end
-
-ClientHWID = CheckExecutor({Valyse = true}) and gethwid() or game:GetService('RbxAnalyticsService'):GetClientId()
-setclipboard(ClientHWID)
-
-for k,v in pairs(scriptKeys.validScriptKeys) do
-	if table.find(v, ClientHWID) and script_Key == k then
-		HasKey = true
-		break
-	end
-	if table.find(k, script_Key) then
-		warn("Valid Key")
-		request({
-			Url = 'https://pastebin.com/raw/ymTEAzmy1',
-			Method = 'POST',
-			Headers = {
-				['Content-Type'] = 'application/json'
-			},
-			Body = game:GetService('HttpService'):JSONEncode(scriptKeys.validScriptKeys[script_Key].WhitelistedTo == tostring(ClientHWID))
-		})
-		HasKey = true
-		break
+	ClientHWID = CheckExecutor({Valyse = true}) and gethwid() or game:GetService('RbxAnalyticsService'):GetClientId()
+	for i: number, scriptLink in next, GameList do
+		if i == game.PlaceId then
+			return scriptLink()
+		end
 	end
 end
-
-
-if HasKey then
-	print("loaded")
-else
-	warn("fake key or tried to do something")
-end
-
---pcall(function() local a='https://roblox.com';getgenv().scriptKey="";getgenv().;loadstring(game:HttpGet(a))(end);
