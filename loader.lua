f7fb1ce3f38f0dbdfae516f20300c24456648b02
averyoriginalusername <144381271+averyoriginalusername@users.noreply.github.com>
2023-09-11 19:56:23 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,7 @@
 do	
 	local GameList = {
-		[13747403394] = loadstring(game:HttpGet("https://raw.githubusercontent.com/averyoriginalusername/main/main/Rogue-Lineage-Richest-Minion.lua"));
+		[13747403394] =  loadstring(game:HttpGet("https://raw.githubusercontent.com/averyoriginalusername/main/main/Rogue-Lineage-Richest-Minion.lua"));
+		[7162704734] = loadstring(game:HttpGet("https://raw.githubusercontent.com/averyoriginalusername/main/main/Fighting-Game.lua"));
 	}
 	local ClientHWID = nil
 	local CheckExecutor = function(Table)
@@ -11,10 +12,10 @@ do
 			return false
 		end
 	end
-	print(request({Url = "https://raw.githubusercontent.com/averyoriginalusername/main/main/Rogue-Lineage-Richest-Minion.lua", Method = "GET"}))
 	ClientHWID = CheckExecutor({Valyse = true}) and gethwid() or game:GetService('RbxAnalyticsService'):GetClientId()
 	for i: number, scriptLink in next, GameList do
 		if i == game.PlaceId then
+			print("ye")
 			return scriptLink()
 		end
 	end
