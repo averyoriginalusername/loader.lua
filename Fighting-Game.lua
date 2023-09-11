1cacface89d19c5f5754e798e6845ff416b02aad
averyoriginalusername <144381271+averyoriginalusername@users.noreply.github.com>
2023-09-11 19:55:56 +0100

    Create Fighting-Game.lua

---
+++ Fighting-Game.lua
@@ -0,0 +1,164 @@
+local Player = game.Players.LocalPlayer
+local Character = Player.Character or Player.CharacterAdded:Wait()
+local PlayerBackpack = Player:WaitForChild("Backpack")
+local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+local CharacterRemotes = Character:WaitForChild("CharacterHandler"):WaitForChild("Remotes")
+
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+	Name = "Multiware: Fighting Game",
+	LoadingTitle = "Loading: Fighting Game...",
+	LoadingSubtitle = "by ???",
+	ConfigurationSaving = {
+		Enabled = true,
+		FolderName = "multiwareConfig",
+		FileName = "mwFGConfig"
+	},
+})
+
+local ClientTab = Window:CreateTab("Client Cheats", 4483362458) -- Title, Image
+
+
+local GameVisualsTab = Window:CreateTab("Game Visuals", 4483362458) -- Title, Image
+local OldBrightness = game.Lighting.Brightness
+
+local FBToggle = GameVisualsTab:CreateToggle({
+	Name = "Enable Fullbright",
+	CurrentValue = false,
+	Flag = "FBToggle", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Toggle)
+        if Toggle == true then
+            game.Lighting.Brightness = 40
+        else
+            game.Lighting.Brightness = OldBrightness
+        end
+	end,
+})
+local DisableShadowsToggle = GameVisualsTab:CreateToggle({
+	Name = "Disable Shadows",
+	CurrentValue = false,
+	Flag = "ShadowsToggle", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Toggle)
+        local TurnOffShadows = function(Bool)
+            if Bool == true then
+                for i,v in workspace:GetDescendants() do
+                    if v:IsA("BasePart") then
+                        v.CastShadows = Bool and false or true
+                        game.Lighting.GlobalShadows = Bool and false or true
+                    end
+                end
+            end
+        end
+	
+        TurnOffShadows(Toggle)
+    end,
+})
+
+local Slider = ClientTab:CreateSlider({
+	Name = "Slider Example",
+	Range = {0, 100},
+	Increment = 10,
+	Suffix = "Bananas",
+	CurrentValue = 10,
+	Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Value)
+		-- The function that takes place when the slider changes
+    		-- The variable (Value) is a number which correlates to the value the slider is currently at
+	end,
+})
+local Button = ClientTab:CreateButton({
+	Name = "Button Example",
+	Callback = function()
+		-- The function that takes place when the button is pressed
+	end,
+})
+
+local Toggle = ClientTab:CreateToggle({
+	Name = "Toggle Example",
+	CurrentValue = false,
+	Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Value)
+		-- The function that takes place when the toggle is pressed
+    		-- The variable (Value) is a boolean on whether the toggle is true or false
+	end,
+})
+
+local Slider = ClientTab:CreateSlider({
+	Name = "Slider Example",
+	Range = {0, 100},
+	Increment = 10,
+	Suffix = "Bananas",
+	CurrentValue = 10,
+	Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Value)
+		-- The function that takes place when the slider changes
+    		-- The variable (Value) is a number which correlates to the value the slider is currently at
+	end,
+})
+
+local Label = ClientTab:CreateLabel("Label Example")
+
+local Paragraph = ClientTab:CreateParagraph({Title = "Paragraph Example", Content = "Paragraph Example"})
+
+local Input = ClientTab:CreateInput({
+	Name = "Input Example",
+	PlaceholderText = "Input Placeholder",
+	RemoveTextAfterFocusLost = false,
+	Callback = function(Text)
+		-- The function that takes place when the input is changed
+    		-- The variable (Text) is a string for the value in the text box
+	end,
+})
+
+local Keybind = ClientTab:CreateKeybind({
+	Name = "Keybind Example",
+	CurrentKeybind = "Q",
+	HoldToInteract = false,
+	Flag = "Keybind1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Keybind)
+		-- The function that takes place when the keybind is pressed
+    		-- The variable (Keybind) is a boolean for whether the keybind is being held or not (HoldToInteract needs to be true)
+	end,
+})
+
+local Dropdown = Tab:CreateDropdown({
+	Name = "Dropdown Example",
+	Options = {"Option 1","Option 2"},
+	CurrentOption = "Option 1",
+	Flag = "Dropdown1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Option)
+	  	  -- The function that takes place when the selected option is changed
+    	  -- The variable (Option) is a string for the value that the dropdown was changed to
+	end,
+})
+
+local Button = Tab:CreateButton({
+	Name = "Destroy UI",
+	Callback = function()
+		Rayfield:Destroy()
+	end,
+})
+
+-- Extras
+
+-- getgenv().SecureMode = true -- Only Set To True If Games Are Detecting/Crashing The UI
+
+-- Rayfield:Destroy() -- Destroys UI
+
+-- Rayfield:LoadConfiguration() -- Enables Configuration Saving
+
+-- Section:Set("Section Example") -- Use To Update Section Text
+
+-- Button:Set("Button Example") -- Use To Update Button Text
+
+-- Toggle:Set(false) -- Use To Update Toggle
+
+-- Slider:Set(10) -- Use To Update Slider Value
+
+-- Label:Set("Label Example") -- Use To Update Label Text
+
+-- Paragraph:Set({Title = "Paragraph Example", Content = "Paragraph Example"}) -- Use To Update Paragraph Text
+
+-- Keybind:Set("RightCtrl") -- Keybind (string) -- Use To Update Keybind
+
+-- Dropdown:Set("Option 2") -- The new option value -- Use To Update/Set New Dropdowns
