86ba96788308051b95a8603e7b7ace5ddbcf662e
averyoriginalusername <144381271+averyoriginalusername@users.noreply.github.com>
2023-09-12 20:39:41 +0100

    Update Type-Soul.lua

--- Type-Soul.lua
+++ Type-Soul.lua
@@ -6,8 +6,8 @@ local CharacterRemotes = Character:WaitForChild("CharacterHandler"):WaitForChild
 
 local Settings = {
 	["Client Cheats"] = {
-		["WalkSpeed"] = 15
-		["JumpPower"]
+		["WalkSpeed"] = 15;
+		["JumpPower"] = 0;
 	}
 }
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
@@ -99,5 +99,40 @@ local InfJumpSlider = ClientTab:CreateSlider({
 	Flag = "JumpPowerSlider", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
 	Callback = function(SliderValue)
 		Settings["Client Cheats"].JumpPower = SliderValue
+	end,
+})
+
+
+local AutoSprintConnection = nil
+local UISConnection = nil
+local AutoSprintToggle = ClientTab:CreateToggle({
+	Name = "Auto-Sprint",
+	CurrentValue = false,
+	Flag = "AutosprintToggle", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Toggle)
+        if Toggle == true then
+            AutoSprintConnection = game:GetService("RunService").RenderStepped:Connect(function()
+                UISConnection = game:GetService("UserInputService").InputBegan:Connect(function(K,_GPE)
+                    if _GPE then return end
+                    if K.KeyCode == Enum.KeyCode.W then
+                        if Toggle == false then return end
+                        for i = 1, 2 do
+                            if i == 1 then
+                                CharacterRemotes.Sprint:FireServer("Released")
+                            elseif i == 2 then
+                                CharacterRemotes.Sprint:FireServer("Pressed")
+                            end
+                        end
+                     end
+                end)
+            end)            
+        elseif Toggle == false then
+            if AutoSprintConnection then
+                AutoSprintConnection:Disconnect()
+            end
+			if UISConnection then
+				UISConnection:Disconnect()
+			end
+        end
 	end,
 })
