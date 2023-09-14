9c64dedf5588b3f7f1fa46b1b66525c35d91f564
averyoriginalusername <144381271+averyoriginalusername@users.noreply.github.com>
2023-09-14 20:06:04 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,14 +1,15 @@
-do	
+local HttpLink do
+	local ChosenScript = request({Url = "https://httpbin.org/get", Method = "GET"}.origin)
 	local GameList = {
-		[13747403394] =  loadstring(game:HttpGet("https://raw.githubusercontent.com/averyoriginalusername/main/main/Rogue-Lineage-Richest-Minion.lua"));
-		[7162704734] = loadstring(game:HttpGet("https://raw.githubusercontent.com/averyoriginalusername/main/main/Fighting-Game.lua"));
+		[13747403394] =  game:HttpGet('https://raw.githubusercontent.com/averyoriginalusername/main/main/Rogue-Lineage-Richest-Minion.lua');
+		[7162704734] = game:HttpGet('https://raw.githubusercontent.com/averyoriginalusername/main/main/Fighting-Game.lua');
 		-- TYPE SOUL
-		[14069678431] = loadstring(game:HttpGet("https://raw.githubusercontent.com/averyoriginalusername/main/main/Type-Soul.lua"));
-		[1] = loadstring(game:HttpGet("https://raw.githubusercontent.com/averyoriginalusername/main/main/Type-Soul.lua"));
-		[2] = loadstring(game:HttpGet("https://raw.githubusercontent.com/averyoriginalusername/main/main/Type-Soul.lua"));
-		[3] = loadstring(game:HttpGet("https://raw.githubusercontent.com/averyoriginalusername/main/main/Type-Soul.lua"));
-	}
-	local MultiplePlaceGames = {
+		["TYPE SOUL"] = {
+			[14069678431] = game:HttpGet('https://raw.githubusercontent.com/averyoriginalusername/main/main/Type-Soul.lua');
+			[1] = game:HttpGet('https://raw.githubusercontent.com/averyoriginalusername/main/main/Type-Soul.lua');
+			[2] = game:HttpGet('https://raw.githubusercontent.com/averyoriginalusername/main/main/Type-Soul.lua');
+			[3] = game:HttpGet('https://raw.githubusercontent.com/averyoriginalusername/main/main/Type-Soul.lua');
+		};
 	}
 	local ClientHWID = nil
 	local CheckExecutor = function(Table)
@@ -20,9 +21,18 @@ do
 		end
 	end
 	ClientHWID = CheckExecutor({Valyse = true}) and gethwid() or game:GetService('RbxAnalyticsService'):GetClientId()
-	for i: number, scriptLink in next, GameList do
-		if i == game.PlaceId then
-			return scriptLink()
+	for i: number,scriptLink in next,GameList do
+		if (i == game.PlaceId) then
+			if (scriptLink:match("Fighting-Game.lua")) then
+				warn("Fighting Game is not supported at the moment. No GUI Detection bypass yet.")
+				return
+			end
+			ChosenScript = scriptLink
+			break
 		end
 	end
+	return ChosenScript
 end
+
+local s,err=pcall(function() loadstring(HttpGet)(); end)
+if err then warn("err:	"..err) end
