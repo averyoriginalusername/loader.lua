70b5e3a2c28ea6aeb20e3de18511f17559dfe43e
averyoriginalusername <144381271+averyoriginalusername@users.noreply.github.com>
2023-09-15 17:09:55 +0100

    Update Fighting-Game.lua

--- Fighting-Game.lua
+++ Fighting-Game.lua
@@ -7,98 +7,50 @@ local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
 
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
-	Name = "Rayfield Example Window",
-	LoadingTitle = "Rayfield Interface Suite",
-	LoadingSubtitle = "by Sirius",
+	Name = "Multiware: Fighting Game",
+	LoadingTitle = "Loading: Fighting Game...",
+	LoadingSubtitle = "by ???",
 	ConfigurationSaving = {
 		Enabled = true,
-		FolderName = "Rayfield Interface Suite",
-		FileName = "Big Hub"
+		FolderName = "multiwareConfig",
+		FileName = "mwFGConfig"
 	},
-	KeySystem = false, -- Set this to true to use their key system
-	KeySettings = {
-		Title = "Sirius Hub",
-		Subtitle = "Key System",
-		Note = "Join the discord (discord.gg/sirius)",
-		SaveKey = true,
-		Key = "ABCDEF"
-	}
 })
 
-local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
+local ClientTab = Window:CreateTab("Client Cheats", 4483362458) -- Title, Image
 
-local Section = Tab:CreateSection("Section Example")
 
-local Button = Tab:CreateButton({
-	Name = "Button Example",
-	Callback = function()
-		-- The function that takes place when the button is pressed
-	end,
-})
+local GameVisualsTab = Window:CreateTab("Game Visuals", 4483362458) -- Title, Image
+local OldBrightness = game.Lighting.Brightness
 
-local Toggle = Tab:CreateToggle({
-	Name = "Toggle Example",
+local FBToggle = GameVisualsTab:CreateToggle({
+	Name = "Enable Fullbright",
 	CurrentValue = false,
-	Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-	Callback = function(Value)
-		-- The function that takes place when the toggle is pressed
-    		-- The variable (Value) is a boolean on whether the toggle is true or false
+	Flag = "FBToggle", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Toggle)
+        if Toggle == true then
+            game.Lighting.Brightness = 40
+        else
+            game.Lighting.Brightness = OldBrightness
+        end
 	end,
 })
-
-local Slider = Tab:CreateSlider({
-	Name = "Slider Example",
-	Range = {0, 100},
-	Increment = 10,
-	Suffix = "Bananas",
-	CurrentValue = 10,
-	Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-	Callback = function(Value)
-		-- The function that takes place when the slider changes
-    		-- The variable (Value) is a number which correlates to the value the slider is currently at
-	end,
+local DisableShadowsToggle = GameVisualsTab:CreateToggle({
+	Name = "Disable Shadows",
+	CurrentValue = false,
+	Flag = "ShadowsToggle", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Toggle)
+        local TurnOffShadows = function(Bool)
+            if Bool == true then
+                for i,v in workspace:GetDescendants() do
+                    if v:IsA("BasePart") then
+                        v.CastShadows = Bool and false or true
+                        game.Lighting.GlobalShadows = Bool and false or true
+                    end
+                end
+            end
+        end
+	
+        TurnOffShadows(Toggle)
+    end,
 })
-
-local Label = Tab:CreateLabel("Label Example")
-
-local Paragraph = Tab:CreateParagraph({Title = "Paragraph Example", Content = "Paragraph Example"})
-
-local Input = Tab:CreateInput({
-	Name = "Input Example",
-	PlaceholderText = "Input Placeholder",
-	RemoveTextAfterFocusLost = false,
-	Callback = function(Text)
-		-- The function that takes place when the input is changed
-    		-- The variable (Text) is a string for the value in the text box
-	end,
-})
-
-local Keybind = Tab:CreateKeybind({
-	Name = "Keybind Example",
-	CurrentKeybind = "Q",
-	HoldToInteract = false,
-	Flag = "Keybind1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-	Callback = function(Keybind)
-		-- The function that takes place when the keybind is pressed
-    		-- The variable (Keybind) is a boolean for whether the keybind is being held or not (HoldToInteract needs to be true)
-	end,
-})
-
-local Dropdown = Tab:CreateDropdown({
-	Name = "Dropdown Example",
-	Options = {"Option 1","Option 2"},
-	CurrentOption = "Option 1",
-	Flag = "Dropdown1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-	Callback = function(Option)
-	  	  -- The function that takes place when the selected option is changed
-    	  -- The variable (Option) is a string for the value that the dropdown was changed to
-	end,
-})
-
-local Button = Tab:CreateButton({
-	Name = "Destroy UI",
-	Callback = function()
-		Rayfield:Destroy()
-	end,
-})
-
