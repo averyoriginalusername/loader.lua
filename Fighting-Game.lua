95a4782bcea57f2f77aaa5228045fe6b2283fe46
averyoriginalusername <144381271+averyoriginalusername@users.noreply.github.com>
2023-09-15 17:03:19 +0100

    Update Fighting-Game.lua

--- Fighting-Game.lua
+++ Fighting-Game.lua
@@ -25,8 +25,6 @@ local Window = Rayfield:CreateWindow({
 	}
 })
 
-Rayfield:Notify("Title Example", "Content/Description Example", 4483362458) -- Notfication -- Title, Content, Image
-
 local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
 
 local Section = Tab:CreateSection("Section Example")
