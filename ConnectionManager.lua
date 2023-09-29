de44228cb9ee797c87329b2e34f0821bddb7593b
averyoriginalusername <144381271+averyoriginalusername@users.noreply.github.com>
2023-09-29 20:49:08 +0100

    Create ConnectionManager.lua

---
+++ ConnectionManager.lua
@@ -0,0 +1,23 @@
+local Connections = {}
+Connections.__index = Connections
+
+function Connections.new()
+    local self = setmetatable({}, Connections)
+    self.list = {}
+    return self
+end
+
+function Connections:Create(flag, _ConnectionFunc)
+    table.insert(self.list, {flag = flag, connection = _ConnectionFunc})
+end
+
+function Connections:Disconnect(connection)
+    for i, v in pairs(self.list) do
+        if self.list[i].flag == tostring(connection) then
+            self.list[i].connection:Disconnect()
+            table.remove(self.list, i)
+            return true
+        end
+    end
+    return false
+end
